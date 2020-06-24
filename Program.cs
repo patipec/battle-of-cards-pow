de1395cea28c7cafee3c0c33e1ab431492f00029
Pati <ppeczynska@gmail.com>
2020-06-24 20:12:54 +0200

    add scheme

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace battle_of_cards_pow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
